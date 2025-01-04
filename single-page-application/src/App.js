edbb5a17e187ec76db4e8805a13b7c14f47d71da
Michael Monteblanco <michael.thomas.monteblanco@hotmail.com>
2025-01-03 23:25:18 -0800

    Add basic styling to SPA

--- single-page-application/src/App.js
+++ single-page-application/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from "./components/Home";
 import About from "./components/About";
-
+import './styles/App.css';
 
 const App = () => {
   return (
